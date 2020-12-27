bc9a80d9e21bde7527f442ca1b565f581841a840
John Ervin <49054998+StJohnErvin@users.noreply.github.com>
2020-12-28 07:02:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
-// import dotenv
-const dotenv = require("dotenv");
-dotenv.config();
-
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { readdirSync } = require("fs");
-
+require("dotenv").config();
 
 // app
 const app = express();
 
 // db
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
     useUnifiedTopology: true,
   })
   .then(() => console.log("DB CONNECTED"))

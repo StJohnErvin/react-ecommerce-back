3df9c2b7b8990f7034efb750bf20e4a173b9be93
John Ervin <49054998+StJohnErvin@users.noreply.github.com>
2020-12-29 08:31:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -32,3 +32,4 @@ readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+ 
